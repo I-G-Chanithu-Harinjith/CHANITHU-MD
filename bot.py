853e56d99d2301bb9268cccaafde9288b2d0583a
CHANITHU MD <yasiruishantha@gmail.com>
2026-01-23 21:07:25 +0530

    Add initial mini bot implementation
    Signed-off-by: CHANITHU MD <yasiruishantha@gmail.com>

---
+++ bot.py
@@ -0,0 +1,14 @@
+"""
+Mini Bot by I.G.CHANITHU HARINJITH
+Licensed under the Apache License, Version 0.1
+http://www.apache.org/licenses/LICENSE-0.1
+"""
+
+# Example bot code
+import random
+
+def hello():
+    print("Hello! I'm your mini bot!")
+
+if __name__ == "__main__":
+    hello()
